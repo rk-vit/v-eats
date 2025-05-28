8b7b271c24ff424b0ab6132fde9dc3c7f307a1bd
Revanth Kanna M <revanthkannam05@gmail.com>
2025-05-28 19:19:43 +0530

    Full auth logic & shop schema

--- client/app/lib/db.ts
+++ client/app/lib/db.ts
@@ -1,14 +1,12 @@
 import {Pool} from "pg";
-const db = new Pool({
+/*const db = new Pool({
     user:"postgres",
     host:"localhost",
     database:"V_EATS",
     password:"rk@vit",
     port:5432
-})
-/*
+})*/
 const db = new Pool({
     connectionString:process.env.DATABASE_URL
 })
-    */
 export default db;
